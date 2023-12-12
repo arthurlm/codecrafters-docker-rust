d1a74016da35f795b21810f48b4d7dc038813141
Arthur LE MOIGNE <arthur.lemoigne@gmail.com>
2023-12-12 18:24:53 +0100

    Stage 2: Wireup stdout & stderr

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,22 @@
+use std::{
+    env,
+    process::{exit, Command, Stdio},
+};
+
 use anyhow::{Context, Result};
 
 // Usage: your_docker.sh run <image> <command> <arg1> <arg2> ...
 fn main() -> Result<()> {
-    let args: Vec<_> = std::env::args().collect();
+    let args: Vec<_> = env::args().collect();
     let command = &args[3];
     let command_args = &args[4..];
-    let output = std::process::Command::new(command)
+
+    let mut child = Command::new(command)
         .args(command_args)
-        .output()
+        .stdin(Stdio::inherit())
+        .stdout(Stdio::inherit())
+        .stderr(Stdio::inherit())
+        .spawn()
         .with_context(|| {
             format!(
                 "Tried to run '{}' with arguments {:?}",
@@ -15,12 +24,6 @@ fn main() -> Result<()> {
             )
         })?;
 
-    if output.status.success() {
-        let std_out = std::str::from_utf8(&output.stdout)?;
-        println!("{}", std_out);
-    } else {
-        std::process::exit(1);
-    }
-
-    Ok(())
+    let status = child.wait()?;
+    exit(status.code().unwrap_or(1));
 }
